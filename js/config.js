e9a54f36057fccfe64cceab6c621a65d5cac9941
Estefano Ramirez <estefano.ramirez@pedidosya.com>
2025-09-26 12:28:34 -0300

    Cambio 4

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // IMPORTANTE: Reemplaza con tu número de WhatsApp Business.
     // Incluye el código de país, sin el símbolo '+' ni espacios. Ejemplo: 5491112345678
-    numeroWhatsApp: 'TUNUMERODEWHATSAPP'
+    numeroWhatsApp: '+59897680439'
 };
